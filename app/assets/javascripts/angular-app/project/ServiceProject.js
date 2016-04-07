e737b4dfaf0671a1e7e0435b25a8e7673bd31d7d
kolyastepanets <kolyastepanets@gmail.com>
2016-04-07 10:16:12 +0300

    small fix

--- app/assets/javascripts/angular-app/project/ServiceProject.js
+++ app/assets/javascripts/angular-app/project/ServiceProject.js
@@ -1,7 +1,6 @@
 app.factory('Project', ['$resource', function($resource){
   return $resource('/api/v1/projects/:id', { id: '@id' },
     {
-      'query': { method: "GET", isArray: true },
       'update':  { method: 'PATCH' },
       'destroy': { method: 'DELETE' }
     }
